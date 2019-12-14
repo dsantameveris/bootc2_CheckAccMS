935952591fca4a6065e33db2cdf72246e2f43774
Jaeggler <gonzalo.santacruzm@gmail.com>
2019-12-13 20:04:09 -0500

    Swagger Config

--- src/main/java/com/everis/CheckAccMS/Controller/CheckAccountController.java
+++ src/main/java/com/everis/CheckAccMS/Controller/CheckAccountController.java
@@ -4,6 +4,9 @@ import com.everis.CheckAccMS.Model.CheckAccount;
 import com.everis.CheckAccMS.Service.Impl.CheckAccountServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
@@ -17,18 +20,21 @@ public class CheckAccountController
     private CheckAccountServiceImpl service;
 
     //Create Account
+    @PostMapping("/checkacc/create")
     public Mono<CheckAccount> createAccount(CheckAccount account)
     {
         return service.addAccount(account);
     }
 
     //Delete Account
+    @DeleteMapping("/checkacc/delete")
     public Mono<Void> deleteAccount(CheckAccount account)
     {
         return service.delAccount(account);
     }
 
     //Get all CheckAccounts
+    @GetMapping("/checkacc/all")
     public Flux<CheckAccount> getAllAccounts()
     {
         return service.findAllAccounts();
